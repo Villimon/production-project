3e4c36b62b59825ce213490b0c1786c4a2470398
Villimon <sidorov.oleg98@mail.ru>
2024-05-13 22:26:46 +0300

    до 120 поднял

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,7 +62,7 @@ module.exports = {
             'error',
             {
                 ignoreComments: true,
-                code: 110,
+                code: 120,
             },
         ],
     },
