a4841d870a1cd3f489eb8ee6f0531b25b5269003
Villimon <sidorov.oleg98@mail.ru>
2024-05-18 23:37:20 +0300

    Redux-toolkit. Entity. Тесты на всех уровнях

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,4 +1,6 @@
+import { DeepPartial } from '@reduxjs/toolkit';
 import { render } from '@testing-library/react';
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import React, { ReactNode } from 'react';
 
 import { I18nextProvider } from 'react-i18next';
@@ -7,16 +9,19 @@ import i18n from '../../../config/i18n/i18nForTests';
 
 export interface renderWithRouterOptions {
     route?: string;
+    initialState?: DeepPartial<StateSchema>;
 }
 
 export function componentRender(
     component: ReactNode,
     options: renderWithRouterOptions = {},
 ) {
-    const { route = '/' } = options;
+    const { route = '/', initialState } = options;
     return render(
-        <MemoryRouter initialEntries={[route]}>
-            <I18nextProvider i18n={i18n}>{component}</I18nextProvider>
-        </MemoryRouter>,
+        <StoreProvider initialState={initialState}>
+            <MemoryRouter initialEntries={[route]}>
+                <I18nextProvider i18n={i18n}>{component}</I18nextProvider>
+            </MemoryRouter>
+        </StoreProvider>,
     );
 }
