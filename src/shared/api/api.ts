05f5463dfd49797ac51eeb62a58489eea147ba57
Villimon <sidorov.oleg98@mail.ru>
2024-06-22 17:16:24 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -6,7 +6,14 @@ import { USER_LOCAL_STORAGE_KEY } from 'shared/constants/localstorage';
 export const $api = axios.create({
     // baseURL: baseUrl,
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '',
-    },
+});
+
+// перехватчик который отрабатывает перед запросом
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.authorization =
+            localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
+    }
+
+    return config;
 });
