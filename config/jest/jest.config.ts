aa4b3859c046ad97fad46d048a2aa6d286e2e0c3
Villimon <sidorov.oleg98@mail.ru>
2024-07-02 20:51:36 +0300

    HTML report для тестов

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -34,6 +34,18 @@ export default {
         '\\.(s?css)$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
+    reporters: [
+        'default',
+        [
+            'jest-html-reporters',
+            {
+                publicPath: '<rootDir>/reports/unit',
+                filename: 'report.html',
+                openReport: true,
+                inlineSource: true,
+            },
+        ],
+    ],
     // roots: [
     //   "<rootDir>"
     // ],
--- src/pages/ArticlesPage/ui/ArticleInfiniteList/ArticleInfiniteList.tsx
+++ src/pages/ArticlesPage/ui/ArticleInfiniteList/ArticleInfiniteList.tsx
@@ -1,12 +1,12 @@
 import { ArticleList } from 'entitites/Article';
+import { FC, memo } from 'react';
+import { useSelector } from 'react-redux';
 import {
     getArticlePageError,
     getArticlePageIsLoading,
     getArticlePageView,
 } from '../../model/selectors/articlePageSelectors';
 import { getArticle } from '../../model/slice/articlePageSlice';
-import { FC, memo } from 'react';
-import { useSelector } from 'react-redux';
 
 interface ArticleInfiniteListProps {
     className?: string;
