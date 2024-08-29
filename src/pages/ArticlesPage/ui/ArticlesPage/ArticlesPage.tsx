58fae65c50e2da6c8de36650970acda9a03bb715
SidorovOS <OSeSidorov@sberbank.ru>
2024-08-29 20:47:42 +0300

    Селекторы с аргументами. Типизация buildSelector

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,44 +1,44 @@
-import {
-    FC, memo, useCallback, useEffect,
-} from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { classNames } from '@/shared/lib/classNames/classNames';
+import { FC, memo, useCallback, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { classNames } from '@/shared/lib/classNames/classNames'
 import {
     DynamicModuleLoader,
     ReducersList,
-} from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { Page } from '@/widgets/Page';
-import { fetchNextArticlesPage } from '../../model/services/fetchNextArticlesPage/fetchNextArticlesPage';
-import { initArticlesPage } from '../../model/services/initArticlesPage/initArticlesPage';
-import { articlePageReducer } from '../../model/slice/articlePageSlice';
-import { ArticlesPageFilters } from '../ArticlesPageFilters/ArticlesPageFilters';
-import { ArticleInfiniteList } from '../ArticleInfiniteList/ArticleInfiniteList';
-import cls from './ArticlesPage.module.scss';
+} from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch'
+import { Page } from '@/widgets/Page'
+import { fetchNextArticlesPage } from '../../model/services/fetchNextArticlesPage/fetchNextArticlesPage'
+import { initArticlesPage } from '../../model/services/initArticlesPage/initArticlesPage'
+import { articlePageReducer } from '../../model/slice/articlePageSlice'
+import { ArticlesPageFilters } from '../ArticlesPageFilters/ArticlesPageFilters'
+import { ArticleInfiniteList } from '../ArticleInfiniteList/ArticleInfiniteList'
+import cls from './ArticlesPage.module.scss'
+import { useArticleItemById } from '../../model/selectors/articlePageSelectors'
 
 const reducer: ReducersList = {
     articlePage: articlePageReducer,
-};
+}
 
 interface ArticlesPageProps {
-    className?: string;
+    className?: string
 }
 
 // На странице не должно быть никаких селекторов, запросов, все это должно выноситься в фичи
 // Но так как мы изначально начали делать тут и весь стейт у нас тут, то мы будем делать внутри это директории
 const ArticlesPage: FC<ArticlesPageProps> = memo(({ className }) => {
-    const dispatch = useAppDispatch();
-    const [searchParams] = useSearchParams();
+    const dispatch = useAppDispatch()
+    const [searchParams] = useSearchParams()
+    const articleItem = useArticleItemById('2')
 
     const onLoadNextPart = useCallback(() => {
-        dispatch(fetchNextArticlesPage());
-    }, [dispatch]);
+        dispatch(fetchNextArticlesPage())
+    }, [dispatch])
 
     useEffect(() => {
         if (__PROJECT__ !== 'storybook') {
-            dispatch(initArticlesPage(searchParams));
+            dispatch(initArticlesPage(searchParams))
         }
-    }, [dispatch, searchParams]);
+    }, [dispatch, searchParams])
 
     return (
         <DynamicModuleLoader reducers={reducer} removeAfterUnmount={false}>
@@ -51,7 +51,7 @@ const ArticlesPage: FC<ArticlesPageProps> = memo(({ className }) => {
                 <ArticleInfiniteList className={cls.list} />
             </Page>
         </DynamicModuleLoader>
-    );
-});
+    )
+})
 
-export default ArticlesPage;
+export default ArticlesPage
