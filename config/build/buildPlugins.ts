574f78ec6493ba68fa34fd03a5024a66f6c431a0
SidorovOS <OSeSidorov@sberbank.ru>
2024-10-01 21:10:16 +0300

    eslint fix

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,12 +1,11 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import webpack from 'webpack'
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
-import CopyBlugin from 'copy-webpack-plugin'
-import CircularDependencyPlugin from 'circular-dependency-plugin'
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
-import { BuildOptions } from './types/config'
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import CopyBlugin from 'copy-webpack-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { BuildOptions } from './types/config';
 
 export function buildPlugins({
     paths,
@@ -14,7 +13,7 @@ export function buildPlugins({
     apiUrl,
     project,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-    const isProd = !isDev
+    const isProd = !isDev;
     const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
@@ -25,17 +24,17 @@ export function buildPlugins({
             __API__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project),
         }),
-    ]
+    ];
 
     if (isDev) {
-        plugins.push(new ReactRefreshWebpackPlugin())
-        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(new ReactRefreshWebpackPlugin());
+        plugins.push(new webpack.HotModuleReplacementPlugin());
         plugins.push(
             new CircularDependencyPlugin({
                 exclude: /node_modules/,
                 failOnError: true,
-            })
-        )
+            }),
+        );
         // Плагин который проверяет типы ТС, но не во время сборки а после
         plugins.push(
             new ForkTsCheckerWebpackPlugin({
@@ -45,8 +44,8 @@ export function buildPlugins({
                         syntactic: true,
                     },
                 },
-            })
-        )
+            }),
+        );
         // plugins.push(
         //     new BundleAnalyzerPlugin({
         //         openAnalyzer: false,
@@ -59,15 +58,15 @@ export function buildPlugins({
             new MiniCssExtractPlugin({
                 filename: 'css/[name].[contenthash:8].css',
                 chunkFilename: 'css/[name].[contenthash:8].css',
-            })
-        )
+            }),
+        );
         // для переносов перевод с папку build
         plugins.push(
             new CopyBlugin({
                 patterns: [{ from: paths.locales, to: paths.buildLocales }],
-            })
-        )
+            }),
+        );
     }
 
-    return plugins
+    return plugins;
 }
